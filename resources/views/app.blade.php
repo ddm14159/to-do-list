1c5be1603941bf608d47ceb13f94ab2c4f3e2fb7
Ddm14159 <ddm14159@gmail.com>
2022-07-14 20:28:43 +0300

    heroku setup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,6 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         <!-- Scripts -->
-        @routes
         @vite('resources/js/app.js')
         @inertiaHead
     </head>
